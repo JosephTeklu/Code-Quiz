dd8cbdfaf43ff27a716dc66ae50e121f08e0c6de
Joseph Teklu <jteklu@outlook.com>
2023-03-20 21:40:11 -0600

    updated code

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // variables for keeping time
-var time; var timeLeft = 60;
+var time; var timeLeft = 30;
+
+// array for the high scores
+var arr = [];
 
 // show the time on the webpage
 document.getElementById("timer").textContent = "Time: " + timeLeft + "sec";
@@ -17,44 +20,73 @@ document.getElementById("start").addEventListener("click", function(){
         // show the time on the webpage
         document.getElementById("timer").textContent = "Time: " + timeLeft + "sec";
         // if there is no time left stop the timer
-        if(timeLeft == 0){clearInterval(time);}
+        if(timeLeft < 1){clearInterval(time);}
     },1000);
 });
 
-// find every submit button in the html and assing it to submitButtons
-for (let submitButtons of document.getElementsByClassName("submit")){
-    // if the submit button is clicked fire this event
-    submitButtons.addEventListener("click", function(event){
+// find every answer in the html and assing it to answer
+for (let answer of document.getElementsByClassName("answer")){
+    // if one of the answers have been clicked
+    answer.addEventListener("click", function(event){
         // prevent default
         event.preventDefault();
 
-        // check which submit button is clicked and if the correct answer is also 
-        // selected at the same time go to the next question or the end
-        if(submitButtons.getAttribute("id") == "submit1" && document.getElementById("correct1").checked == true){
-            document.getElementById("question1").style.display = "none";
-            document.getElementById("question2").style.display = "block";
+        // if ther is more than one second left in the timer
+        if(timeLeft >= 1)
+        {
+            // if the current answer clicked is the correct answer
+            if(answer.getAttribute("id"))
+            {
+                // grab the dataset from (current) that is in the current element and use it to hide the current question
+                document.getElementById(answer.dataset.current).style.display = "none";
+                // grab the dataset from (after) that is in the current element and use it to show the next question
+                document.getElementById(answer.dataset.after).style.display = "block";
+                // if the current question is the last one, end the timer, put the score in local storage
+                if(answer.dataset.current == "question5") {clearInterval(time); document.getElementById("score").textContent = timeLeft; window.localStorage.setItem("hs1",timeLeft);}
+            }
+            // if the corret answer was not clicked
+            else{
+                // grab the dataset from (current) that is in the current element and use it to hide the current question
+                document.getElementById(answer.dataset.current).style.display = "none";
+                // grab the dataset from (after) that is in the current element and use it to show the next question
+                document.getElementById(answer.dataset.after).style.display = "block";
+                // if the current question is the last one, end the timer, show the user their score, put the score in local storage
+                if(answer.dataset.current == "question5") {clearInterval(time); document.getElementById("score").textContent = timeLeft; window.localStorage.setItem("hs2",timeLeft);}
+                // decrement by 4 seconds
+                timeLeft -= 4;
+            }
         }
-        if(submitButtons.getAttribute("id") == "submit2" && document.getElementById("correct2").checked == true){
-            document.getElementById("question2").style.display = "none";
-            document.getElementById("question3").style.display = "block";
-        }
-        if(submitButtons.getAttribute("id") == "submit3" && document.getElementById("correct3").checked == true){
-            document.getElementById("question3").style.display = "none";
-            document.getElementById("question4").style.display = "block";
-        }
-        if(submitButtons.getAttribute("id") == "submit4" && document.getElementById("correct4").checked == true){
-            document.getElementById("question4").style.display = "none";
-            document.getElementById("question5").style.display = "block";
-        }
-        if(submitButtons.getAttribute("id") == "submit5" && document.getElementById("correct5").checked == true){
-            document.getElementById("question5").style.display = "none";
+        // else if there is no time left on the clock jump to the end
+        else{
+            // grab the dataset from (current) that is in the current element and use it to hide the current question
+            document.getElementById(answer.dataset.current).style.display = "none";
+            // show the high score page
             document.getElementById("end").style.display = "block";
-            // end the timer
+            // show the user their score
+            document.getElementById("score").textContent = timeLeft; 
+            // stop the timer
             clearInterval(time);
+            // show the user their score
+            document.getElementById("score").textContent = timeLeft; 
+            // put the score in local storage
+            window.localStorage.setItem("hs3",timeLeft);
         }
 
-        // if none of the corret answers were choosen decrease the time by 4
-        timeLeft -= 4;
-
-    }); 
+    });
 }
+
+// and event that would fire if the user clicked the submit score button
+document.getElementById("submitScore").addEventListener("click", function(event){
+    // prevent default
+    event.preventDefault();
+    
+    // stop showing the end of the quiz
+    document.getElementById("end").style.display = "none";
+    // show the high scores list 
+    document.getElementById("highScoresList").style.display = "block";
+
+    arr.push(window.localStorage.getItem("hs1"));
+    arr.push(window.localStorage.getItem("hs2"));
+    arr.push(window.localStorage.getItem("hs3"));
+
+});
